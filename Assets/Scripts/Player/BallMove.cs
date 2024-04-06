3220107889bb8bc3c5788c1d59fe9ae1f40adc86
ICouldCode <brygriff06@gmail.com>
2024-04-06 01:08:09 -0500

    Finished 3 PowerUps

--- Assets/Scripts/Player/BallMove.cs
+++ Assets/Scripts/Player/BallMove.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMove : MonoBehaviour
@@ -9,33 +7,76 @@ public class BallMove : MonoBehaviour
     [SerializeField] LayerMask groundMask;
     readonly float distance = 0.2f;
     
-    float fallMultiplier = 2.5f; 
-    float lowJumpMultiplier = 2f;
-    bool Grounded;
+    readonly float fallMultiplier = 2.5f; 
+    readonly float lowJumpMultiplier = 2f;
+    
+    private bool Grounded;
+    [HideInInspector]public bool canMove = true;
 
     float Speed {  get; set; }
     float Jump { get; set; }
+
+    public enum Gravity{
+        UP, DOWN
+    }
+
+    [HideInInspector]public Gravity gravity;
+
+    [HideInInspector]public float AbilityDuration = -1;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         Speed = 3;
         Jump = 10;
+        gravity = Gravity.UP;
     }
 
     private void Update()
     {
-        //Movement
-        if(Input.GetAxis("Horizontal") > 0) { rb.AddForce(Vector3.right * Speed); }
-        if (Input.GetAxis("Horizontal") < 0) { rb.AddForce(-Vector3.right * Speed); }
-        if (Input.GetAxis("Vertical") > 0) { rb.AddForce(Vector3.forward * Speed); }
-        if (Input.GetAxis("Vertical") < 0) { rb.AddForce(-Vector3.forward * Speed); }
+        if (!canMove) { return; }
 
+        Movement();
+        if(AbilityDuration > -1) { TimedAbilities(); }
+    }
+
+    private void TimedAbilities()
+    {
+        AbilityDuration -= Time.deltaTime;
+        if(AbilityDuration < 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void Movement()
+    {
+        //Movement
+        if (gravity == Gravity.UP)
+        {
+            if (Input.GetAxis("Horizontal") > 0) { rb.AddForce(Vector3.right * Speed); }
+            if (Input.GetAxis("Horizontal") < 0) { rb.AddForce(-Vector3.right * Speed); }
+            if (Input.GetAxis("Vertical") > 0) { rb.AddForce(Vector3.forward * Speed); }
+            if (Input.GetAxis("Vertical") < 0) { rb.AddForce(-Vector3.forward * Speed); }
+        }
+        else
+        {
+            if (Input.GetAxis("Horizontal") > 0) { rb.AddForce(-Vector3.right * Speed); }
+            if (Input.GetAxis("Horizontal") < 0) { rb.AddForce(Vector3.right * Speed); }
+            if (Input.GetAxis("Vertical") > 0) { rb.AddForce(Vector3.forward * Speed); }
+            if (Input.GetAxis("Vertical") < 0) { rb.AddForce(-Vector3.forward * Speed); }
+        }
+
+        JumpM();
+    }
+
+    private void JumpM()
+    {
         //Jump
         groundObj.transform.position = transform.position - (Vector3.up / 2);
         Grounded = Physics.CheckSphere(groundObj.position, distance, groundMask);
         if (Input.GetButtonDown("Jump") && Grounded) { rb.AddForce(Vector3.up * Jump, ForceMode.Impulse); }
         if (rb.velocity.y < 0) { rb.velocity += (fallMultiplier - 1) * Physics.gravity.y * Time.deltaTime * Vector3.up; }
-        else if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump")) { rb.velocity += (lowJumpMultiplier - 1) * Physics.gravity.y * Time.deltaTime * Vector3.up; }
+        else if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump")) { rb.velocity += (lowJumpMultiplier - 1) * Physics.gravity.y * Time.deltaTime * Vector3.up; } 
     }
 }
---
+++ Assets/Scripts/PowerUpScripts/Power.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PowerUps/CreatePowerUp")]
+public class Power : ScriptableObject
+{
+    public GameObject Player;
+    public Sprite powerUpIcon;
+    public string powerUpName;
+    public float powerUpMultiplier;
+    
+    public void UsePower()
+    {
+        switch (powerUpName)
+        {
+            case "Rocket Jump":
+                Player.GetComponent<Rigidbody>().AddForce(Vector3.up * powerUpMultiplier, ForceMode.Impulse);
+                break;
+            case "Antigravity":
+                Physics.gravity *= -1;
+                if(Physics.gravity.y > 0.0f)
+                {
+                    Camera.main.transform.rotation = Quaternion.Euler(new Vector3(-20, 0, 180));
+                    Player.GetComponent<BallMove>().gravity = BallMove.Gravity.DOWN;
+                }
+                else
+                {
+                    Camera.main.transform.rotation = Quaternion.Euler(new Vector3(20, 0, 0));
+                    Player.GetComponent<BallMove>().gravity = BallMove.Gravity.UP;
+                }
+                Camera.main.transform.GetComponent<CameraLook>().Offset.y *= -1;
+                break;
+            case "SpeedBoost":
+                if (Input.GetAxis("Horizontal") > 0) { Player.transform.position += new Vector3(powerUpMultiplier * Time.deltaTime, 0.1f, 0.0f); }
+                else if (Input.GetAxis("Horizontal") < 0) { Player.transform.position += new Vector3(-powerUpMultiplier * Time.deltaTime, 0.1f, 0.0f); }
+
+                if (Input.GetAxis("Vertical") > 0) { Player.transform.position += new Vector3(0.0f , 0.0f, powerUpMultiplier * Time.deltaTime); }
+                else if (Input.GetAxis("Vertical") < 0) { Player.transform.position += new Vector3(0.0f, 0.0f, -powerUpMultiplier * Time.deltaTime); }
+
+                break;
+            case "MegaSphere":
+                Player.GetComponent<BallMove>().AbilityDuration = powerUpMultiplier;
+                Player.transform.localScale *= 5f;
+                break;
+
+        }
+    }
+}
