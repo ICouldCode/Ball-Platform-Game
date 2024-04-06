3ad665b5b4f660be4a0665d2ec7c196a461e2677
ICouldCode <brygriff06@gmail.com>
2024-04-06 02:21:50 -0500

    Title screen revamp

---
+++ Assets/Menu/Scripts/MenuBallRotating.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBallRotating : MonoBehaviour
+{
+    Vector3 rot;
+
+    private void Awake()
+    {
+        rot = Vector3.up;
+    }
+
+    private void Update()
+    {
+        rot.y += Time.deltaTime;
+        transform.rotation = Quaternion.Euler(rot);   
+    }
+}
